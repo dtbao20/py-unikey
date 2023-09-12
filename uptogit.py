ffcbce2c5a1f191e3d9361d879e7d225ff9ae90e
baodinhaaa <dinhbao2000@gmail.com>
2023-09-12 21:29:02 +0700

    ,,

--- uptogit.py
+++ uptogit.py
@@ -1,15 +1,19 @@
 import os
 os.chdir( os.path.abspath(os.getcwd()) )
 
+# os.system('git add --all -- :!uptogit.py')
 os.system('git add .')
-os.system('git reset -- main/uptogit.py')
 os.system('git commit -m ",,"')
-os.system('git push')
-
+# os.system('git push')
+os.system('git branch -M main')
+os.system('git remote add origin "https://github.com/dtbao20/py-unikey.git"')
+os.system('git push -u origin main')
 
 
 # os.system('git init')
 # os.system('git add .')
+# os.system('git reset -- uptogit.py')
+# os.system('git add --all -- :!path/to/file1 :!path/to/file2 :!path/to/folder1/*')
 # os.system('git commit -m "%s"' % ",,")
 # os.system('git branch -M main')
 # os.system('git remote add origin ""https://github.com/xxx/xxx.git')
